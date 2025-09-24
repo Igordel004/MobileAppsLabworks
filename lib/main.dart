4ad845c329841a5f6295c72bcbb3eac82df22b8a
IgorTokarev <igordel004@gmail.com>
2025-09-24 17:24:48 +0400

    k (как же так)

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
       ),
-      home: const MyHomePage(title: 'Pocemons'),
+      home: const MyHomePage(title: 'Pokemons'),
     );
   }
 }
